9296f347fa62f0e9231a28d50d39b0f6c07d72cd
LucioFider <fidelislucio404@gmail.com>
2023-04-09 15:48:45 +1000

    Added ToasterProvider

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -9,6 +9,8 @@ import useRegisterModal from "@/app/hooks/useRegisterModal";
 import Modal from "./Modal";
 import Heading from "../Heading";
 import Input from "../inputs/Input";
+import { toast } from "react-hot-toast";
+import Button from "../Button";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -34,7 +36,7 @@ const RegisterModal = () => {
         registerModal.onClose();
       })
       .catch((error) => {
-        console.log(error);
+        toast.error("Something went wrong!");
       })
       .finally(() => {
         setIsLoading(false);
@@ -52,6 +54,35 @@ const RegisterModal = () => {
         errors={errors}
         required
       />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        type="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  );
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        onClick={() => {}}
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+      />
     </div>
   );
 
@@ -64,7 +95,9 @@ const RegisterModal = () => {
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
+      footer={footerContent}
     />
   );
 };
+
 export default RegisterModal;
