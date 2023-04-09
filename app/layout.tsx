1d907e2e90eb86dd87d32bdaf1021b2db1f075a3
LucioFider <fidelislucio404@gmail.com>
2023-04-09 10:14:34 +1000

    Created the Modal popup for Login and Sign up

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal />
+          <Modal title="Hello Airbnb" isOpen />
           <Navbar />
         </ClientOnly>
         {children}
