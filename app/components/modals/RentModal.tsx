911ce6a5c72a5ac73ca015030975d38e1e5d9742
LucioFider <fidelislucio404@gmail.com>
2023-04-12 12:39:53 +1000

    Created the Rent Modal component

---
+++ app/components/modals/RentModal.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React from "react";
+import Modal from "./Modal";
+import useRentModal from "@/app/hooks/useRentModal";
+
+const RentModal = () => {
+  const rentModal = useRentModal();
+  return (
+    <Modal
+      isOpen={rentModal.isOpen}
+      title="Sello your home"
+      onClose={rentModal.onClose}
+      onSubmit={rentModal.onClose}
+      actionLabel="Submit"
+    />
+  );
+};
+
+export default RentModal;
