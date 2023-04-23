1eaec314c45f64ef65aa5348d6e229eb647eb736
LucioFider <fidelislucio404@gmail.com>
2023-04-23 20:55:52 +1000

    Created the Properties listings

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,15 @@ import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
-import getListings from "./actions/getListings";
+import getListings, { IListingsParams } from "./actions/getListings";
 import getCurrentUser from "./actions/getCurrentUser";
 
-export default async function Home() {
-  const listings = await getListings();
+interface HomeProps {
+  searchParams: IListingsParams;
+}
+
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
   // const isEmpty = true;
 
@@ -35,4 +39,5 @@ export default async function Home() {
       </Container>
     </ClientOnly>
   );
-}
+};
+export default Home;
